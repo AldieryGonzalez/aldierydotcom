5ca4fe6fd329237f0398141ddb66e4e36ad56fc8
Aldiery Gonzalez <aldieryrgon@gmail.com>
2024-03-12 00:42:24 -0500

    Feat: Simiplified boxes

--- src/components/ui/background-boxes.tsx
+++ src/components/ui/background-boxes.tsx
@@ -37,36 +37,21 @@ export const BoxesCore = ({ className, ...rest }: { className?: string }) => {
           key={`row` + i}
           className="relative h-16  w-32  border-l border-slate-700"
         >
-          {cols.map((_, j) => (
-            <motion.div
-              whileHover={{
-                backgroundColor: `var(${getRandomColor()})`,
-                transition: { duration: 0 },
-              }}
-              animate={{
-                transition: { duration: 2 },
-              }}
-              key={`col` + j}
-              className="relative h-16 w-32 border-r border-t border-slate-700"
-            >
-              {j % 2 === 0 && i % 2 === 0 ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="pointer-events-none absolute -left-[22px] -top-[14px] h-6 w-10 stroke-[1px] text-slate-700"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 6v12m6-6H6"
-                  />
-                </svg>
-              ) : null}
-            </motion.div>
-          ))}
+          {cols.map((_, j) => {
+            return (
+              <motion.div
+                whileHover={{
+                  backgroundColor: `var(${getRandomColor()})`,
+                  transition: { duration: 0.125 },
+                }}
+                animate={{
+                  transition: { duration: 2 },
+                }}
+                key={`col` + j}
+                className="relative h-16 w-32 border-r border-t border-slate-700"
+              />
+            );
+          })}
         </motion.div>
       ))}
     </div>
